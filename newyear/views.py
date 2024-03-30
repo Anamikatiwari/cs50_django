3a24ee51cae3618555e00b96245c7d4353ec9f11
Anamika Tiwari <155213271+Anamikatiwari@users.noreply.github.com>
2024-03-30 20:37:24 +0545

    added newyear

---
+++ newyear/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+
+
+def index(request):
+    now= datetime.datetime.now()
+    return render(request, "newyear/index.html", {
+        
+        # this below code give the output NO
+        # "newyear":now.month ==  1 and now.day == 1
+        
+        # this below code give the output YES
+        "newyear": True
+    })
+    
